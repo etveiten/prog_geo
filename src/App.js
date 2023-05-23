7f31c7d49f75c5f15a5da5cfecdca9a16654f30d
etveiten <erlenstv@stud.ntnu.no>
2023-05-23 15:03:24 +0200

    Sidebar component UI added, needs styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Map from "./Map/Map";
-
+import Sidebar from "./Components/Sidebar";
 function App() {
   return (
     <div className="App">
-      <div className="sidebar-container"></div>
+      <div className="sidebar-container">
+        <Sidebar />
+      </div>
       <div className="map-container">
         <Map />
       </div>
