75ab337a11b7f5de3e9843e0bdc137880e83d6f9
Erlend Solheim Tveiten <est@dhcp-10-22-35-132.wlan.ntnu.no>
2023-06-17 16:22:14 +0200

    #20: added folding of containers, added tools button and icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import "./App.css";
 import Map from "./Components/Map/Map";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { useState } from "react";
 
 // Contexts
 import { MapProvider } from "./Components/Map/MapContext";
 import { MapSettingsProvider } from "./Components/Sidebar/MapSettings/MapSettingsContext";
 import { LayersProvider } from "./Components/Sidebar/Layers/LayersContext"; // Updated import
 
+//Add icons
+import {ReactComponent as ToolsIcon} from "./Icons/tools-svgrepo-com.svg";
+
 function App() {
+  const [isSidebarFolded, setIsSidebarFolded] = useState(false);
+  const [isToolsFolded, setIsToolsFolded] = useState(true);
+
+  const handleToolsFoldButtonClick = () => {
+    setIsToolsFolded(!isToolsFolded);
+  };
+
+
   return (
     <MapProvider>
       <MapSettingsProvider>
         <LayersProvider>
           <div className="App">
-            <div className="sidebar-container">
+            <div
+              className="sidebar-container"
+            >
               <Sidebar />
             </div>
             <div className="map-container">
               <Map />
+            </div>
+            <div className={`tools-container ${isToolsFolded ? "folded" : ""}`}>
+              <button
+                className={`fold-button tools-button ${
+                  isToolsFolded ? "folded" : ""
+                }`}
+                onClick={handleToolsFoldButtonClick}
+              >
+                <span className="icon">
+                <ToolsIcon className="tools-icon" />
+                </span>
+              </button>
+              {/* Add your content for the tools container here */}
             </div>
           </div>
         </LayersProvider>
