d5e43610cc3fe622ee6abbfbed94f4c85159fb06
Erlend Solheim Tveiten <est@Erlends-MacBook-Pro.local>
2023-09-21 13:14:47 +0200

    before push force of new master

--- src/Utils/InfoMessages.js
+++
@@ -1,9 +0,0 @@
-export const bufferInfoMessage = "This is the info message for Buffer.";
-
-export const differenceInfoMessage = "This is the info message for Difference.";
-
-export const intersectInfoMessage = "This is the info message for Intersect.";
-
-export const unionInfoMessage = "This is the info message for Union.";
-
-export const toolsInfoMessage = "Buffer: Creating a protective zone or boundary around an object or area, like drawing a buffer zone around a nature reserve to limit development. Difference: Identifying and extracting the unique elements or features that exist in one dataset but not in another, such as finding the differences between two sets of geographical boundaries. Intersect: Determining the common or overlapping areas between two or more datasets or layers, useful for identifying shared characteristics or spatial relationships. Union: Merging or combining multiple datasets or layers into a single dataset, resulting in a unified representation of the combined features or areas from the individual datasets."
